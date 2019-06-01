2d59840feaf5619efde057679b24835086a73f0f
sapatelbaps <sapatelbaps@gmail.com>
2019-05-31 23:33:33 -0700

    Exercise file to generate random number is added!

--- BasicInterfaces/Program.cs
+++ BasicInterfaces/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BasicInterfaces
+﻿namespace BasicInterfaces
 {
     class Program
     {
@@ -13,6 +7,7 @@ namespace BasicInterfaces
             // BasicInterfaceEx.Start();
             // MultipleInterfaces.Start();
             // NetInterfaces.Start();
+            RandomNumGenerator.Start();
 
         }
     }
---
+++ BasicInterfaces/RandomNumGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BasicInterfaces
+{
+    class RandomNumGenerator : IRandomizable
+    {
+        public static void Start()
+        {
+            Console.WriteLine("Enter 'exit' anytime to close the program.");
+            string input = null;
+
+            do
+            {
+                Console.WriteLine("Please enter integer number - of the maximum range to get Random Number: ");
+                input = Console.ReadLine();
+
+                // If input is empty then continue;
+                input = input ?? "";
+
+                if (input.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                int upperBound = 0;
+                if (int.TryParse(input, out upperBound))
+                {
+                    var cls = new RandomNumGenerator();
+                    Console.WriteLine(cls.GetRandomNum(upperBound));
+                }
+
+            } while (input.ToLower() != "exit");
+        }
+
+        public float GetRandomNum(int upperBound)
+        {
+            var random = new Random(new Random().Next());
+            random.Next(0, upperBound);
+            return float.Parse(random.Next().ToString());
+        }
+
+    }
+
+    interface IRandomizable
+    {
+        float GetRandomNum(int upperBound);
+    }
+
+}
