cb4472fb9103994b370617dacd95b5a8c5a84ce1
sapatelbaps <sapatelbaps@gmail.com>
2019-05-31 20:31:15 -0700

    Example files extracted.

---
+++ Exercise Files/Ch03/03_01/IntroGenerics/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroGenerics
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            int total = 0;
+
+
+            foreach (int i in arrList) {
+                total += i;
+            }
+            Console.WriteLine("The total is {0}\n\n", total);
+
+            Console.WriteLine("Press Enter to continue");
+            Console.ReadLine();
+        }
+    }
+}
